c17bca54e6bbd316c157013daf5286b590aa46d5
Carrie Miskin <clove40@gmail.com>
2022-10-06 14:25:38 -0400

    added to-do form in frontend

--- client/src/todo/Todo.js
+++ client/src/todo/Todo.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import '../todo/Todo.css'
+
+const handleSubmit = (e) =>{
+  e.preventDefault();
+}
 
 const Todo = () => {
+
+
   return (
-    <h2>Todo List</h2>
+  <div>
+    <h2>Create a To-Do List </h2>
+      <form onSubmit={ handleSubmit } className='form-container'>
+          <div>
+            <label> Weekday: </label>
+            <input type="text" name="week-day" id='week-day' />
+          </div>
+          <input type="submit" value="Create To-Do List" />
+        </form>
+  </div>
   )
 }
 
